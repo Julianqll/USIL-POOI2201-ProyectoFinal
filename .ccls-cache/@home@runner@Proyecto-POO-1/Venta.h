dfdaf6d87dada3b96690e1cff4a64246463d3881
Julianqll <64666002+Julianqll@users.noreply.github.com>
2022-06-26 18:43:15 +0000

    feat: venta class and methods added

---
+++ .ccls-cache/@home@runner@Proyecto-POO-1/Venta.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "Producto.h"
+
+using namespace std;
+
+class Venta {
+private:
+    vector<Producto> productos;
+    string cliente;
+public:
+    Venta()
+    {
+
+    }
+    ~Venta() {
+
+    }
+    Venta(vector<Producto> productos, string cliente)
+    {
+        this->productos = productos;
+        this->cliente = cliente;
+    }
+    // metodos
+
+    void imprimirBoleta()
+    {
+        cout << "Detalle de compra: "<< endl;
+        int sum = 0;
+        for (Producto producto : productos)
+        {
+            cout << "Item: " << producto.getnombre() << " Cantidad: " << producto.getcantidad() << " Total: " << producto.getcantidad()*producto.getprecio() <<  endl;
+            sum += producto.getcantidad()*producto.getprecio();
+        }
+        cout << "Importe total: " << sum << endl;
+    }
+};
